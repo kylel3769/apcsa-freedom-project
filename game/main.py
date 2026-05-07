932d1ae56b108202d50fd5e28103670b842d50f6
Kyle Lam <146866570+kylel3769@users.noreply.github.com>
2026-05-07 09:36:32 -0400

    fix indent smh

--- game/main.py
+++ game/main.py
@@ -83,8 +83,8 @@ async def main():
                         elif enemy_location == "5": enemy_location = random.choice(["2", "3"])
                         elif enemy_location == "3":
                             enemy_location = random.choice(["Office", "4", "5"])
-                                if enemy_location = "Office"
-                                    game_over = True 
+                            if enemy_location = "Office"
+                                game_over = True 
 
             if event.type == pygame.MOUSEBUTTONDOWN:
                 print(f'({mc[0]}, {mc[1]}, 60, 30)}}')
